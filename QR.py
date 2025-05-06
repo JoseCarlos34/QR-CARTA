c391a4f1ff13b977669a179fb99d42ef83e183d0
Jose Carlos <cuentatrabajo2605@gmail.com>
2025-05-06 12:55:51 -0500

    Primera versión con QR inicial

--- QR.py
+++ QR.py
@@ -1,7 +1,7 @@
 import qrcode
 
-url = "http://192.168.100.15/restaurante/"
+url = "https://qr-carta.vercel.app/carta.php"
 img = qrcode.make(url)
 img.save("qr_carta.png")
 
-print("✅ Código QR generado con la IP 192.168.100.15")
+print("✅ Código QR generado con la URL pública")
